49421c01a8f1f23e748229d5218384d2b1fc24bf
anseohyun <60808168+anseohyun@users.noreply.github.com>
2021-09-29 14:10:27 +0900

    diary db

--- app/src/main/java/com/example/chatbot/DBHelper.java
+++ app/src/main/java/com/example/chatbot/DBHelper.java
@@ -5,31 +5,37 @@ import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import java.sql.Blob;
+import java.util.ArrayList;
+
 public class DBHelper extends SQLiteOpenHelper {
 
     String sql;
 
+    private static final int DB_VERSION = 1;
+    private static final String DB_NAME = "chatbotDB.db";
     private static final String TAG = "DBHelper";
     private SQLiteDatabase db;
 
     public DBHelper(Context context) {
-        super(context, "chatbotDB", null, 1);
+        super(context, DB_NAME, null, DB_VERSION);
     }
 
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        // 다이어리 테이블
-        sql = "CREATE TABLE diary_posts (post_id INTEGER PRIMARY KEY AUTOINCREMENT,";
-        sql += "reporting_date INTEGER NOT NULL, weather INTEGER, content TEXT, img_file BLOB)";
+        //데이터베이스가 생성이 될 때 호출출
+        //데이터베이스 -> 테이블 -> 컬럼 -> 값
 
-        db.execSQL(sql);
+       // 다이어리 테이블
+        db.execSQL("CREATE TABLE DiaryData (post_id INTEGER PRIMARY KEY AUTOINCREMENT, reporting_date CHAR(10) NOT NULL, content TEXT, img_file BYTE)");
 
         // 투두 테이블
         sql = "create table diary_todo ("
@@ -50,13 +56,60 @@ public class DBHelper extends SQLiteOpenHelper {
 
     }
 
+    //SELECT 문 (할일 목록 조회)
+    public ArrayList<DiaryData> getDiaryData() {
+        ArrayList<DiaryData> diaryDatas = new ArrayList<>();
+
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM DiaryData ORDER BY reporting_date DESC", null);
+
+        if(cursor.getCount() != 0){
+            //조회온 데이터가 있을때 내부 수행
+            while(cursor.moveToNext()){
+
+                int post_id = cursor.getInt(cursor.getColumnIndex("post_id"));
+                String reporting_date = cursor.getString(cursor.getColumnIndex("reporting_date"));
+                String content = cursor.getString(cursor.getColumnIndex("content"));
+                byte[] img_file = cursor.getBlob(cursor.getColumnIndex("img_file"));
+
+                DiaryData diaryData = new DiaryData();
+                diaryData.setPost_id(post_id);
+                diaryData.setContent(content);
+                diaryData.setReporting_date(reporting_date);
+                diaryData.setImg(img_file);
+            }
+       }
+        cursor.close();
+
+        return diaryDatas;
+    }
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
 
-        db.execSQL("DROP TABLE diary_posts;");
+        db.execSQL("DROP TABLE DiaryData;");
         db.execSQL("DROP TABLE diary_todo");
         db.execSQL("DROP TABLE recipe");
         this.onCreate(db);
+    }
+
+    //INSERT문 (할일 목록을 DB에 넣는다.)
+    //img_file의 타입???
+    public void InsertDiary(String _reporting_date, String _content, byte[] _img_file){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("INSERT INTO DiaryData (reporting_date, content, img_file) VALUES('"+ _reporting_date + "','" + _content + "' , '" + _img_file + "');");
+    }
+
+    //diary_update문 (할일 목록 수정 한다.)
+    public void updateDiary(String _reporting_date, String _content, byte[] _img_file, Integer _post_id){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("UPDATE DiaryData SET content='" + _content + "', reporting_date='" + _reporting_date +  "', img_file='" + _img_file + "' WHERE post_id='" + _post_id + "'");
+    }
+
+    // DELETE 문(Diary를 제거한다.)
+    public void deleteDiary(int _post_id){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DELETE FROM DiaryData WHERE post_id '" + _post_id + "'");
     }
 
     @Override
--- app/src/main/java/com/example/chatbot/DiaryData.java
+++ app/src/main/java/com/example/chatbot/DiaryData.java
@@ -4,20 +4,52 @@ import android.graphics.Bitmap;
 
 public class DiaryData {
 
-    Integer id = 0;
-    Integer reporting_date = 0;
-    Integer weather = 0;
+    Integer post_id = 0;
+    String reporting_date;
+    //Integer weather = 0;
     String content = "";
-    Bitmap img = null;
+    byte[] img = null;      //데이터형??
 
-    DiaryData() {}
+    DiaryData(){
 
-    DiaryData(Integer id, Integer date, Integer weather, String content, Bitmap img){
-
-        this.id = id;
-        this. reporting_date = date;
-        this. weather = weather;
+        this.post_id = post_id;
+        this. reporting_date = reporting_date;
+        //this. weather = weather;
         this.content = content;
         this.img = img;
     }
+
+    public Integer getPost_id() {
+        return post_id;
+    }
+
+    public void setPost_id(Integer post_id) {
+        this.post_id = post_id;
+    }
+
+    public String getReporting_date(){
+        return reporting_date;
+    }
+
+    public void setReporting_date(String reporting_date){
+        this.reporting_date = reporting_date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public byte[] getImg() {
+        return img;
+    }
+
+    public void setImg(byte[] img) {
+        this.img = img;
+    }
+
+
 }
